cff62a1d657006639ddcd7755a451eb041655e44
Huzaif Ghori <75994967+Huzaifa1910@users.noreply.github.com>
2022-09-04 18:12:06 +0500

    Update Currency Notes Counter.java

--- Currency Notes Counter.java
+++ Currency Notes Counter.java
@@ -2,7 +2,8 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 class Scratch {
-    public static void main(String[] args) {
+    public static void currency(){
+        System.out.println("Enter Your Amount: ");
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int[] emp ;
@@ -25,11 +26,32 @@ class Scratch {
             }
         }
 //        Arrays.toString(emp);
-        System.out.println(Arrays.toString(emp));
+        System.out.println("Currency Notes Are: " + Arrays.toString(emp));
         int sum = 0;
         for(int i = 0 ; i < emp.length; i++){
             sum += emp[i];
         }
-        System.out.println(sum);
+        System.out.println("The total amount is: " + sum);
+    }
+    public static void main(String[] args) {
+
+        boolean flag = true;
+        boolean flagCheck = true;
+        Scanner usr = new Scanner(System.in);
+        while(flag){
+        if(flagCheck == true){
+        System.out.println("Press 1 for check notes: ");
+        System.out.println("Press 2 for Exit: ");
+            flagCheck = false;
+        }
+        int usrInpt = usr.nextInt();
+        if(usrInpt == 1){
+        currency();
+        System.out.println("Press 1 to Check Again? and 2 to exit");
+        }
+        else if(usrInpt == 2){
+            flag = false;
+        }
+        }
     }
 }
